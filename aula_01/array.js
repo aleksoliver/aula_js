59046dcc1eb7df82ce129359c57fc73fd827aba5
ricardyd1982 <ricardyd@hotmail.com>
2023-05-04 21:47:13 -0300

    final da aula

---
+++ aula_01/array.js
@@ -0,0 +1,12 @@
+ let frutas = []
+
+ // adicionando itens no array
+ frutas.push('Banana', 'Maçã')
+
+ console.log(frutas)
+
+ //remover o primeiro elemento
+
+ frutas.shift()
+
+ console.log(frutas)
